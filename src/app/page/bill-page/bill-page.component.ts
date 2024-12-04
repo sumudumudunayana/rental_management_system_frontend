dd911afc07b3e4105578522bba8f51a1c09cbf5b
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2024-12-04 18:06:09 +0530

    new commit

--- src/app/page/bill-page/bill-page.component.ts
+++ src/app/page/bill-page/bill-page.component.ts
@@ -1,59 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 import { Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import{ Router } from '@angular/router';
 
 import Swal from'sweetalert2';
 
 @Component({
   selector: 'app-bill-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,FormsModule,CommonModule,HttpClientModule],
   templateUrl: './bill-page.component.html',
   styleUrl: './bill-page.component.css'
 })
-export class BillPageComponent {
+export class BillPageComponent implements OnInit {
 
-  public rental: any = {
-    rentalDate: '',
-    returnDate: '',
-    dueDate: '',
-    totalCost: '',
-    customerName: '',
-    itemId: '',
-    itemName: '',
-  };
+  rental: any;
 
-  @Input() customerName!: string;
-  @Input() itemName!: string;
-  @Input() totalCost!: number;
+  constructor(private route: ActivatedRoute) {}
 
-  printBill() {
-    const billDiv = document.querySelector('.bill-container');
-    if (billDiv) {
-      const printContents = billDiv.innerHTML;
-      const originalContents = document.body.innerHTML;
+  ngOnInit(): void {
+    this.rental = history.state.rental;
 
-      document.body.innerHTML = printContents;
-      window.print();
-      document.body.innerHTML = originalContents;
-      window.location.reload();
-    }
-  }
-
-  showBill = false;
-
-generateBill() {
-  if (!this.rental.customerName || !this.rental.itemName || !this.rental.totalCost) {
-    Swal.fire({
-      title: "Missing Information",
-      text: "Please provide all details before generating the bill.",
-      icon: "error",
-    });
-    return;
-  }
-
-  this.showBill = true; // Display the bill component
-}
-
+    if (!this.rental) {
+      
+      window.history.back();
+    }
+  }
 
 }
