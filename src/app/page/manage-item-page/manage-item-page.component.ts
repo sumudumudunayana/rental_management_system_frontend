9bf342db80e31105ba799b39d233fb4bd93217cb
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2024-12-04 15:40:15 +0530

    new commit

--- src/app/page/manage-item-page/manage-item-page.component.ts
+++ src/app/page/manage-item-page/manage-item-page.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-manage-item-page',
@@ -12,5 +13,98 @@ import { RouterLink } from '@angular/router';
   styleUrl: './manage-item-page.component.css'
 })
 export class ManageItemPageComponent {
+
+  public searchQuery: string = '';
+
+  public itemList:any = [];
+
+  constructor(private http: HttpClient) {
+    this.loadTable();
+  }
+
+  loadTable(){
+    this.http.get("http://localhost:8080/hardwareItem/get-all").subscribe(data =>{
+      console.log(data);
+      this.itemList = data;
+    })
+  }
+
+  deleteBook(itemId: number) {
+    this.http.delete(`http://localhost:8080/hardwareItem/delete-by-id/${itemId}`).subscribe(data => {
+      this.deleteSuccessAlert();
+      this.loadTable();
+    })
+  }
+
+  public itemTemp:any = {}
+  updateBook(item:any) {
+    console.log(item);
+    this.itemTemp = item;
+  }
+
+  saveBook(){
+    this.http.put("http://localhost:8080/hardwareItem/update-hardwareItem",this.itemTemp).subscribe(data => {
+      this.updateSuccessAlert();
+      this.loadTable();
+    })
+  }
+
+  searchBook(searchQuery: string) {
+    if (!searchQuery || searchQuery.trim() === '') {
+      this.loadTable();
+      return;
+    }
+    const itemId = parseInt(searchQuery);
+
+    if (isNaN(itemId)) {
+      this.errorAlert();
+      return;
+    }
+
+    this.http.get(`http://localhost:8080/hardwareItem/search-by-id/${itemId}`)
+      .subscribe({
+        next: (response: any) => {
+          console.log('Search response:', response);
+          if (response) {
+            this.itemList = [response];
+          } else {
+            this.itemList = [];
+            this.noBookErrorAlert();
+          }
+        }, 
+      });
+  }
+
+  public deleteSuccessAlert(){
+    Swal.fire({
+      title: "Item Deleted Successfully?",
+      icon: "success",
+      background:"#fff",
+    });
+  }
+
+  public updateSuccessAlert(){
+    Swal.fire({
+      title: "Item Updated Successfully?",
+      icon: "success",
+      background:"#fff",
+    });
+  }
+
+  public errorAlert(){
+    Swal.fire({
+      title: "Please enter a valid numeric ID",
+      icon: "error",
+      background:"#fff",
+    });
+  }
+
+  public noBookErrorAlert(){
+    Swal.fire({
+      title: "No Item found with this ID",
+      icon: "question",
+      background:"#fff",
+    });
+  }
 
 }
--- src/app/page/manage-rental-page/manage-rental-page.component.ts
+++ src/app/page/manage-rental-page/manage-rental-page.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-manage-rental-page',
   standalone: true,
@@ -12,5 +12,98 @@ import { RouterLink } from '@angular/router';
   styleUrl: './manage-rental-page.component.css'
 })
 export class ManageRentalPageComponent {
+
+  public searchQuery: string = '';
+
+  public rentalList:any = [];
+
+  constructor(private http: HttpClient) {
+    this.loadTable();
+  }
+
+  loadTable(){
+    this.http.get("http://localhost:8080/rental/get-all").subscribe(data =>{
+      console.log(data);
+      this.rentalList = data;
+    })
+  }
+
+  deleteBook(rentId: number) {
+    this.http.delete(`http://localhost:8080/rental/delete-by-id/${rentId}`).subscribe(data => {
+      this.deleteSuccessAlert();
+      this.loadTable();
+    })
+  }
+
+  public rentalTemp:any = {}
+  updateBook(rental:any) {
+    console.log(rental);
+    this.rentalTemp = rental;
+  }
+
+  saveBook(){
+    this.http.put("http://localhost:8080/rental/update-rental",this.rentalTemp).subscribe(data => {
+      this.updateSuccessAlert();
+      this.loadTable();
+    })
+  }
+
+  searchBook(searchQuery: string) {
+    if (!searchQuery || searchQuery.trim() === '') {
+      this.loadTable();
+      return;
+    }
+    const rentId = parseInt(searchQuery);
+
+    if (isNaN(rentId)) {
+      this.errorAlert();
+      return;
+    }
+
+    this.http.get(`http://localhost:8080/rental/search-by-id/${rentId}`)
+      .subscribe({
+        next: (response: any) => {
+          console.log('Search response:', response);
+          if (response) {
+            this.rentalList = [response];
+          } else {
+            this.rentalList = [];
+            this.noBookErrorAlert();
+          }
+        }, 
+      });
+  }
+
+  public deleteSuccessAlert(){
+    Swal.fire({
+      title: "Rental Deleted Successfully?",
+      icon: "success",
+      background:"#fff",
+    });
+  }
+
+  public updateSuccessAlert(){
+    Swal.fire({
+      title: "Rental Updated Successfully?",
+      icon: "success",
+      background:"#fff",
+    });
+  }
+
+  public errorAlert(){
+    Swal.fire({
+      title: "Please enter a valid numeric ID",
+      icon: "error",
+      background:"#fff",
+    });
+  }
+
+  public noBookErrorAlert(){
+    Swal.fire({
+      title: "No Rental found with this ID",
+      icon: "question",
+      background:"#fff",
+    });
+  }
 
 }
